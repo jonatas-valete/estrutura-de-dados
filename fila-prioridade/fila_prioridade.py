5c38425b50929e0a62f437a1e744eef32ec545d8
jonatas-valete <jonatasfelippe@gmail.com>
2024-09-12 00:55:29 -0300

    arquivos prontos para adicionar ao github

--- fila-prioridade/fila_prioridade.py
+++ fila-prioridade/fila_prioridade.py
@@ -1,8 +1,9 @@
 class No:
-    def __init__(self, idade):
+    def __init__(self, idade, prioridade='Padrão'):
         self.idade = idade
         self.proximo = None
-        self.prioridade = 'Padrão'
+        self.prioridade = prioridade
+
     def __repr__(self):
         return f'{self.idade}'
         
@@ -10,35 +11,65 @@ class No:
 class Fila:
     def __init__(self):
         self.cabeca = None
-        self.tail = None
+        self.atender = []
+        #self.tail = None
         
-    def append(self, idade):
-        no = No(idade)
+    def append(self, idade, prioridade='Padrão'):
+        no = No(idade, prioridade)
         if not self.cabeca:
             self.cabeca = no
-        if self.tail:
-            self.tail.proximo = no
-        self.tail = no
-           # self.cabeca.proximo = self.tail
-        #else:   
-        if no.idade >= 65:
-            no.prioridade = 'urgente'
-            if self.cabeca.prioridade == 'urgente':
-                no.proximo = self.tail
-                self.tail = no
-            else:
-                no.proximo = self.cabeca
-                self.cabeca = no
+        #self.tail = no
         else:
-            self.tail = no
+            no.proximo = self.cabeca
+            self.cabeca = no
+
+    def ordenar_fila(self):
+        mostrar = []
+        fila = self.cabeca
+        while fila:
+            mostrar.append(fila)
+            fila = fila.proximo
+        fila_ordenada = []
+        while mostrar:
+            i = mostrar.pop()
+            fila_ordenada.append(i)        
+        return fila_ordenada
 
     def mostrar(self):
-        mostrar = []
-        while self.cabeca:
-            mostrar.append(self.cabeca.idade)
-            self.cabeca = self.cabeca.proximo
-        return mostrar
+        pass
+
+    def fila_prioridade(self, fila_ordenada):
+        fila_preferencial = []
+        fila_padrao = []
+        for i in fila_ordenada:
+            if i.prioridade == 'Preferencial':
+                fila_preferencial.append(i)
+            else:
+                fila_padrao.append(i)
+        return fila_preferencial + fila_padrao
+
+    def atender_fila(self):
+        if len(self.atender) == 0:
+            pilha = self.ordenar_fila()
+            fila = self.fila_prioridade(pilha)
+            self.atender = fila
+        else:
+            del self.atender[0]
+            self.atualizar_cabeca()
+        return self.atender
+        
+    def atualizar_cabeca(self):
+        self.cabeca = None
+        for i in range(len(self.atender)):
+            no = No(i)
+            if not self.cabeca:
+                self.cabeca = no
+            else: 
+                no.proximo = self.cabeca
+                self.cabeca = no            
+        
+        
+
+     
         
             
-         
-        
